55f87e23b04b65480fbd58c1093614ef7ccf4ddb
HIP24 <halilpamuk24@gmail.com>
2023-06-23 13:47:15 +0200

    example landmark detection

--- src/EKF_Loc.cpp
+++ src/EKF_Loc.cpp
@@ -130,16 +130,8 @@ public:
   robot_quat_z = msg->pose.pose.orientation.z;
   robot_quat_w = msg->pose.pose.orientation.w;
     // std::cout << "Robot's position: (" << robot_x << ", " << robot_y << ")" << std::endl;
-  }
 
-  // scan callback function
-  void scanCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
-{
-  ++scanPrintCount;
-  z_t.clear();
-  double current_angle = msg->angle_min;
-
-  // Convert robot's orientation quaternion to yaw angle
+      // Convert robot's orientation quaternion to yaw angle
   tf2::Quaternion q;
   q.setX(0);
   q.setY(0);
@@ -150,28 +142,15 @@ public:
 
   // Update theta with the current yaw angle
   theta = yaw;
-
-  for (const auto &range : msg->ranges)
-  {
-    // if range is valid
-    if (range > msg->range_min && range < msg->range_max)
-    {
-      // Convert polar coordinates to Cartesian coordinates
-      double x = range * cos(current_angle);
-      double y = range * sin(current_angle);
-
-      // Convert points from robot's frame to map frame using rotation matrix
-      double map_x = robot_x + cos(yaw)*x - sin(yaw)*y;
-      double map_y = robot_y + sin(yaw)*x + cos(yaw)*y;
-
-      z_t.emplace_back(map_x, map_y);
-    }
-    current_angle += msg->angle_increment;
   }
 
+  // scan callback function
+  void scanCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
+{
+  ++scanPrintCount;
+  
   
 }
-
 
   void printMtCov()
   {
@@ -219,178 +198,12 @@ public:
 
   void correctionStep()
 {
-    // correction
-    // Define a vector of correspondence variables
-    std::vector<std::string> c_t;
-
-    // Print the size of z_t
-    std::cout << "\r\nz_t size: " << z_t.size() << "\r\n" << std::endl;
-
-    if (!z_t.empty())
-    {
-      // Define a map to keep track of the number of times each landmark was chosen
-      std::map<std::string, int> landmark_counts;
-
-      // Set the maximum distance threshold for the closest landmark
-      double max_distance = 10.0;
-
-      for (const auto &measurement : z_t)
-      {
-        // Find the landmark closest to the measurement
-        double min_distance = std::numeric_limits<double>::max();
-        std::string closest_landmark;
-        for (const auto &landmark : landmarks)
-        {
-          double dx = measurement.first - landmark.second.landX;
-          double dy = measurement.second - landmark.second.landY;
-          double distance = sqrt(dx * dx + dy * dy);
-          if (distance < min_distance)
-          {
-            min_distance = distance;
-            closest_landmark = landmark.first;
-          }
-        }
-
-        // Check if the minimum distance is below the maximum distance threshold
-        if (min_distance < max_distance)
-        {
-          // Add the closest landmark to c_t
-          c_t.push_back(closest_landmark);
-
-          // Increment the counter for the closest landmark
-          ++landmark_counts[closest_landmark];
-
-          // Print the correspondence
-          std::cout << "Measurement: (" << measurement.first << ", " << measurement.second << ") -> Landmark: " << closest_landmark << ", Distance: " << min_distance << std::endl;
-        }
-      }
-
-        // Print the number of times each landmark was chosen
-        for (const auto &landmark_count : landmark_counts)
-        {
-          std::cout << "Landmark: " << landmark_count.first << " -> Count: " << landmark_count.second << std::endl;
-        }
-
-      // Set the minimum count threshold for each landmark
-      int min_count = 200;
-
-      // Find the landmark with the highest count above the minimum count threshold
-      int max_count = 0;
-      std::string best_landmark;
-      for (const auto &landmark_count : landmark_counts)
-      {
-        if (landmark_count.second > max_count && landmark_count.second > min_count)
-        {
-          max_count = landmark_count.second;
-          best_landmark = landmark_count.first;
-        }
-      }
-
-
-      // Check if a best landmark was found
-      if (!best_landmark.empty())
-      {
-        // Keep only the measurements associated with the best landmark in z_t
-        auto it_z = z_t.begin();
-        auto it_c = c_t.begin();
-        while (it_z != z_t.end() && it_c != c_t.end())
-        {
-          if (*it_c != best_landmark)
-          {
-            it_z = z_t.erase(it_z);
-            it_c = c_t.erase(it_c);
-          }
-          else
-          {
-            ++it_z;
-            ++it_c;
-          }
-        }
-
-        // Keep only the best landmark in c_t
-        c_t.erase(std::remove_if(c_t.begin(), c_t.end(), [&](const std::string &landmark) { return landmark != best_landmark; }), c_t.end());
-
-        std::cout << "filtered z_t size: " << z_t.size() << std::endl;
-        std::cout << "filtered c_t size: " << c_t.size() << std::endl;
-
-        if(c_t.size() !=0 && c_t.size() == z_t.size()){
-          
-        // Call the correct method with z_t and c_t as arguments
-        //correct(z_t, c_t);
-        //std::cout << "\n\r############## After correction ##############" << std::endl;
-        //printMtCov();
-        }
-        else  std::cout << "Landmark detected but distance too high" << std::endl;
-      }
-    }
-std::cout << "---------------------------------------------" << std::endl;
-}
-
-  std::pair<Eigen::VectorXd, Eigen::MatrixXd> predict(const Eigen::Vector2d &ut, const Eigen::VectorXd &mt_prev, const Eigen::MatrixXd &cov_prev)
-  {
-    std::cout << "Theta:\r\n " << theta << std::endl;
-    std::cout << "v:\r\n " << u_t(0) << std::endl;
-    std::cout << "w:\r\n " << u_t(1) << std::endl;
-    //std::cout << "A:\r\n " << A << std::endl;
-    //std::cout << "B:\r\n " << B << std::endl;
-
-    A.resize(3, 3);
-    A << 1, 0, -u_t[0] * delta_t * sin(theta),
-         0, 1, u_t[0]* delta_t * cos(theta),
-         0, 0, 1;
-
-    mt(0) += u_t[0] * delta_t * cos(theta);
-    mt(1) += u_t[0] * delta_t * sin(theta); 
-    mt(2) += u_t[1] * delta_t; 
-    mt(2) = std::atan2(std::sin(mt(2)), std::cos(mt(2)));
-
-    B.resize(3, 2);
-    B << u_t[0] * delta_t * cos(theta), 0,
-         u_t[0] * delta_t * sin(theta), 0,
-         0, u_t[1] * delta_t;
-
-    //mt = A * mt_prev + B * ut;              // prediction
-    cov = A * cov_prev * A.transpose() + R; // update error covariance
-    return std::make_pair(mt, cov);
+    
   }
 
   void correct(const std::vector<std::pair<double, double>> &z_t, const std::vector<std::string> &c_t)
 {
-  if (!z_t.empty())
-  {
-    for (int i = 0; i < z_t.size(); ++i)
-    {
-      // Determine the index of the landmark corresponding to the observed feature
-      std::string j = c_t[i];
 
-      // Calculate the vector Δ as the difference between the landmark position and the robot position
-      Eigen::Vector2d delta;
-      delta << landmarks[j].landX - mt(0), landmarks[j].landY - mt(1);
-
-      // Calculate the variable q as the squared magnitude of Δ
-      double q = delta.squaredNorm();
-
-      // Calculate the expected measurement ^zit based on Δ and q
-      Eigen::Vector2d expected_z;
-      expected_z << sqrt(q), atan2(delta(1), delta(0));
-
-      // Calculate the Jacobian matrix Hit based on Δ and q
-      Eigen::Matrix<double, 2, 3> H;
-      H << -delta(0) / sqrt(q), -delta(1) / sqrt(q), 0,
-            delta(1) / q, -delta(0) / q, 0;
-
-      // Calculate the Kalman gain Kit based on Hit, Qt, and Σt
-      Eigen::Matrix<double, 3, 2> K = cov * H.transpose() * (H * cov * H.transpose() + Q).inverse();
-
-      // Compute the innovation
-      Eigen::Vector2d z(z_t[i].first, z_t[i].second);
-      Eigen::Vector2d innovation = z - expected_z;
-
-      // Compute the posterior state
-      mt = mt + K * innovation;
-      cov = (Eigen::MatrixXd::Identity(3, 3) - K * H) * cov;
-    }
-  }
 }
 
   void poseEstimation()
